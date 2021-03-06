6611c40d29a336d11181840e54c79ee38aaec9f5
Yogesh Agarwala <69758664+yogeshiitm@users.noreply.github.com>
2021-03-06 19:19:19 +0530

    latex

---
+++ Latex/install_commands_ubuntu.sh
@@ -0,0 +1,7 @@
+sudo apt-get install tcl-dev tk-dev python-tk python3-tk 
+sudo apt-get install python3-pip
+sudo pip3 install numpy
+sudo pip3 install scipy
+sudo pip3 install sympy
+sudo pip3 install matplotlib
+
